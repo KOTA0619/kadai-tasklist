e0aca4df5e71e2d851610d68a3f09ef2a8e22e2f
KOTA <kkou0619@gmail.com>
2020-04-21 05:43:01 +0000

    add column

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_19_113845) do
+ActiveRecord::Schema.define(version: 2020_04_20_083407) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
 end
