714e6016bbc160ccd7e1e3a391cc653ae708ccc4
KOTA <kkou0619@gmail.com>
2020-04-28 06:39:03 +0000

    login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,20 +1,22 @@
 class TasksController < ApplicationController
 before_action :set_task, only: [:show, :edit, :update, :destroy,]
- 
+before_action :require_user_logged_in
+before_action :correct_user, only: [:show, :edit, :update, :destroy]
+
   
   def index
-    @tasks = Task.all
+    @tasks = current_user.tasks.order(id: :desc).page(params[:page]).per(10)
   end
   
   def show
   end
   
   def new
-    @task = Task.new
+    @task = current_user.tasks.build
   end
   
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save
       flash[:success] = 'タスクが正常に作成されました。'
@@ -44,15 +46,23 @@ before_action :set_task, only: [:show, :edit, :update, :destroy,]
     flash[:success] = 'Taskは正常に削除されました。'
     redirect_to tasks_url
   end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
 end
 
-private
-
-def set_task
-  @task = Task.find(params[:id])
-end
-
-def task_params
-  params.require(:task).permit(:content, :status)
-end
 
